c2c0284f5e9d2a62b5f3a8f3acd3f557f0a44c57
Eugen Freund <Eugen.Freund@outlook.de>
2023-09-08 14:46:16 +0200

    Add class War

--- src/viking.js
+++ src/viking.js
@@ -51,4 +51,88 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack() {
+        let randomVikingArrayPositon = Math.floor(Math.random() * this.vikingArmy.length);
+        let attackingViking = this.vikingArmy[randomVikingArrayPositon]
+
+        let randomSaxonArrayPosition = Math.floor(Math.random() * this.saxonArmy.length);
+        let attackedSaxon = this.saxonArmy[randomSaxonArrayPosition];
+
+        let retval = attackedSaxon.receiveDamage(attackingViking.attack());
+
+        if(attackedSaxon.health <= 0 ) {
+            this.saxonArmy.splice(randomSaxonArrayPosition, 1 )
+        }
+        
+        return retval; 
+    }
+
+    saxonAttack() {
+
+        let randomVikingArrayPositon = Math.floor(Math.random() * this.vikingArmy.length);
+        let attackedViking = this.vikingArmy[randomVikingArrayPositon]
+
+        let randomSaxonArrayPosition = Math.floor(Math.random() * this.saxonArmy.length);
+        let attackingSaxon = this.saxonArmy[randomSaxonArrayPosition];
+
+        let retval = attackedViking.receiveDamage(attackingSaxon.attack());
+
+        if(attackedViking.health <= 0 ) {
+            this.vikingArmy.splice(randomSaxonArrayPosition, 1 )
+        }
+        
+        return retval; 
+    }
+
+    showStatus() {
+        if( this.saxonArmy.length < 1 ) {
+            return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length < 1) {
+            return "Saxons have fought for their lives and survived another day...";
+        } else {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
+    }
+
+    battle( attacker , attacked) {
+        let retval = attacked.receiveDamage(attacker.attack());
+    }
+
+
+}
+
+
+let viking1 = new Viking("Eugen", 100, 50);
+let saxon1 = new Saxon(100, 10);
+
+
+
+
+let war1 = new War();
+
+war1.addViking(viking1);
+war1.addSaxon(saxon1);
+
+console.log(war1.saxonArmy);
+console.log(war1.vikingArmy)
+
+console.log(war1.saxonAttack());
+console.log(war1.saxonAttack());
+
+
+console.log(war1.saxonArmy);
+console.log(war1.vikingArmy)
